fc64e97fd4164f6a16aacf3bf268b95fba406bf1
Matheus Bonnet <82242779+MatheusBonnet@users.noreply.github.com>
2021-06-18 17:48:56 -0300

    Update 100a1.cpp

--- 100a1.cpp
+++ 100a1.cpp
@@ -7,5 +7,23 @@ int main(){
 	{
 		cout << "\n";
 		cout << i;
-	}	
+	}
+	
+	int num = 100;
+	
+	while(num >= 1){
+		cout << "\n";
+		cout << num;
+		num--;
+	}
+	
+	int num = 100;
+	
+	do{
+		cout << "\n";
+		cout << num;
+		num--;
+	}while(num >= 1);
+	
+	return 0;
 }
